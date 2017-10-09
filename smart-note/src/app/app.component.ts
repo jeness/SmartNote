c127ef49ad9daf305621c2682e2de68b57d8ac6e
Shanfang <shanfangzhao@gmail.com>
2017-10-09 17:06:16 -0400

    update the appcomponent to handle asynchronous API calls

--- smart-note/src/app/app.component.ts
+++ smart-note/src/app/app.component.ts
@@ -1,33 +1,59 @@
-import {Component} from '@angular/core';
-import {NoteDataService} from './note-data.service';
+import { Component, OnInit } from '@angular/core';
+import { NoteDataService } from './note-data.service';
+import { Note } from './note';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  //providers: [NoteDataService]
+  providers: [NoteDataService]  
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
+
+  notes: Note[] = [];
 
   constructor(
     private noteDataService: NoteDataService
   ) {
   }
 
+  public ngOnInit() {
+    this.noteDataService
+      .getAllNotes()
+      .subscribe(
+        (notes) => {
+          this.notes = notes;
+        }
+      );
+  }
+
   onAddNote(note) {
-    this.noteDataService.addNote(note);
+    this.noteDataService
+      .addNote(note)
+      .subscribe(
+        (newNote) => {
+          this.notes = this.notes.concat(newNote);
+        }
+      );
   }
 
   onToggleNoteComplete(note) {
-    this.noteDataService.toggleNoteComplete(note);
+    this.noteDataService
+      .toggleNoteComplete(note)
+      .subscribe(
+        (updatedNote) => {
+          note = updatedNote;
+        }
+      );
   }
-
   onRemoveNote(note) {
-    this.noteDataService.deleteNoteById(note.id);
+    this.noteDataService
+      .deleteNoteById(note.id)
+      .subscribe(
+        (_) => {
+          this.notes = this.notes.filter((n) => n.id !== note.id);
+        }
+      );
   }
-
-  get notes() {
-    return this.noteDataService.getAllNotes();
-  }
-
 }

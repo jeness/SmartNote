91f46d2e065adbc52beaa087de7c36801663b268
Shanfang <Shanfang@users.noreply.github.com>
2017-10-14 08:59:38 -0400

    Revert "Add default Log In page and update UI"

--- smart-note/src/app/home/home.component.spec.ts
+++ smart-note/src/app/home/home.component.spec.ts
@@ -1,7 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement, NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { HomeComponent } from './home.component';
-
+//import { Note } from '../note';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -9,7 +11,10 @@ describe('HomeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [ HomeComponent ],
+      schemas: [
+        NO_ERRORS_SCHEMA
+      ]
     })
     .compileComponents();
   }));
@@ -24,3 +29,4 @@ describe('HomeComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+

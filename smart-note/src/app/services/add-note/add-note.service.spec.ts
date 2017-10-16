cf199b24267adedcb299b843ace35da5816a533f
Shanfang <shanfangzhao@gmail.com>
2017-10-16 14:35:10 -0400

    fix add note service unit test failure

--- smart-note/src/app/services/add-note/add-note.service.spec.ts
+++ smart-note/src/app/services/add-note/add-note.service.spec.ts
@@ -1,12 +1,18 @@
 import { TestBed, inject } from '@angular/core/testing';
-
 import { AddNoteService } from './add-note.service';
+import { ApiService } from './../../api.service';
+import { ApiMockService } from './../api-mock/api-mock.service';
 
 describe('AddNoteService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [AddNoteService]
-    });
+      providers: [
+        AddNoteService, 
+        {
+          provide:ApiService,
+          useClass: ApiMockService       
+        }
+      ]    });
   });
 
   it('should be created', inject([AddNoteService], (service: AddNoteService) => {

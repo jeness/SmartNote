cd05372e206227f1fac468cae6e411d22aa792e4
Haoran Yu <yuhaoran9968@gmail.com>
2017-10-19 01:41:14 -0400

    save last commit changes

--- smart-note/src/app/login/login.component.ts
+++ smart-note/src/app/login/login.component.ts
@@ -4,6 +4,10 @@ import { environment } from '../../environments/environment';
 import { MsgService } from './../services/msg/msg.service';
 import { HttpClient } from '@angular/common/http';
 
+// import * as $ from "jquery";
+// import { URLSearchParams,Http } from '@angular/http';
+
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -18,7 +22,7 @@ export class LoginComponent implements OnInit {
     private http: HttpClient,
     private msg: MsgService,
     private router: Router,
-    private http: Http
+    // private http: Http
   ) { }
 
   ngOnInit() {
@@ -40,3 +44,27 @@ export class LoginComponent implements OnInit {
     });
   }
 }
+
+// login() {
+//   //$(function(){alert('Hello');});
+//   // var jname = document.getElementById("name");
+//   // var jpwd = document.getElementById("pwd");
+//   // console.log(jname);
+//   // console.log(jpwd);
+//   var user = JSON.stringify({
+//     username:this.name, 
+//     password:this.pwd
+//  });
+//  if(user.length<30){
+//    alert("Invalid user infomation!")
+//   } else{
+//       this.http.post('http://localhost:3000/todos',user).subscribe(
+//       data => {
+//         //alert(this.name);
+//         this.router.navigate(['http://localhost:4200/notes'])
+//       },
+//       error => {
+//         console.log(JSON.stringify(error.json()));})
+//       }
+//   }
+// }

1bc23a4fbb0886afc35e94373ebdf69ff4e901ec
CindyHXH <hxhcindy@gmail.com>
2017-11-01 21:25:15 -0400

    update environment

--- smart-note/src/environments/environment.prod.ts
+++ smart-note/src/environments/environment.prod.ts
@@ -1,5 +1,9 @@
+// export const environment = {
+//   production: true,
+//   // URL of production API
+//   apiUrl: 'http://localhost:3000'
+// };
+
 export const environment = {
-  production: true,
-  // URL of production API
-  apiUrl: 'http://localhost:3000'
+  production: true
 };

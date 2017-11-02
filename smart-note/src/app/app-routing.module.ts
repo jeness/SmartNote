2b41c520b110a64930a9a127977d1b45f8e9464b
CindyHXH <hxhcindy@gmail.com>
2017-11-01 22:00:57 -0400

    add search/archive features

--- smart-note/src/app/app-routing.module.ts
+++ smart-note/src/app/app-routing.module.ts
@@ -1,71 +1,64 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
-import { NotesComponent } from './notes/notes.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { EditNoteComponent } from './page/edit-note/edit-note.component';
+import { ViewNoteComponent } from './page/view-note/view-note.component';
+import { ClassificationComponent } from './page/classification/classification.component';
+import { TagComponent } from './page/tag/tag.component';
+import { SearchComponent } from './page/search/search.component';
+import { AddLinkNoteComponent } from './page/add-link-note/add-link-note.component';
+import { IndexComponent } from './page/index/index.component';
 import { AppComponent } from './app.component';
-import { NotesResolver } from './notes.resolver';
-import { AddNoteComponent } from './add-note/add-note.component';
-import { TagComponent } from './tag/tag.component';
-import { LinkNoteComponent } from './link-note/link-note.component';
-import { AboutComponent } from './about/about.component';
+import { AddNoteComponent } from './page/add-note/add-note.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-    {
-        path: "login" ,
-        component: LoginComponent
-    },
-    {
-        path: "",
-        redirectTo: "/login",
-        pathMatch: "full"
-    },
-    {
-        path: "notes",
-        component: NotesComponent,
-        resolve: {
-            notes: NotesResolver
-        }
-    },
-    {
-        path: 'addNote',
-        component: AddNoteComponent,
-        children: []
-      },
-      {
-        path: "tag",
-        component: TagComponent,
-        children: []
-      },
-    {
-        path: 'linkNote',
-        component: LinkNoteComponent,
-        children: []
-    },
-    { 
-        path: 'about', 
-        component: AboutComponent 
-    },
-    {
-        path: "home",
-        redirectTo: "notes",
-        pathMatch: "full",
-        //component: HomeComponent
-    },
-    {
-        path: "**",
-        component: PageNotFoundComponent
-    }
+  {
+    path: 'index',
+    component: IndexComponent
+  },
+  {
+    path: 'addNote',
+    component: AddNoteComponent,
+    children: []
+  },
+  {
+    path: 'addLinkNote',
+    component: AddLinkNoteComponent,
+    children: []
+  },
+  {
+    path: 'tag',
+    component: TagComponent,
+    children: []
+  },
+  {
+    path: 'classification',
+    component: ClassificationComponent,
+    children: []
+  },
+  {
+    path: 'search',
+    component: SearchComponent,
+    children: []
+  },
+  {
+    path: 'viewNote',
+    component: ViewNoteComponent,
+    children: []
+  },
+  {
+    path: 'editNote',
+    component: EditNoteComponent,
+    children: []
+  },
+  {
+    path: '**',
+    redirectTo: 'index',
+    pathMatch: 'full'
+  }
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule],
-    providers: [
-      NotesResolver
-    ],
-    declarations: []
-  })
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
 export class AppRoutingModule { }
--- smart-note/src/app/tag.ts
+++
@@ -1,9 +0,0 @@
-export class Tag {
-    id: number;
-    title: string = '';
-    complete: boolean = false;
-    
-    constructor(values: Object = {}) {
-      Object.assign(this, values);
-    }
-  }

50e1b2606a87b3c290e66b80210f0346f9ed37dc
Haoran Yu <yuhaoran9968@gmail.com>
2017-11-04 21:21:58 -0400

    Add unit test for note.service

--- smart-note/src/app/services/note/note.service.spec.ts
+++ smart-note/src/app/services/note/note.service.spec.ts
@@ -1,10 +1,14 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { NoteService } from './note.service';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 describe('NoteService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [
+        HttpClientModule
+      ],
       providers: [NoteService]
     });
   });

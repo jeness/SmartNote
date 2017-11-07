c7ec68a5167810625beda569561327dc0e133e9a
CindyHXH <hxhcindy@gmail.com>
2017-11-07 00:03:18 -0500

    Update Unit test and REST API

--- smart-note/models/Todo.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-var TodoSchema = new mongoose.Schema({
-  loginname: String,
-  password: String,
-  //note: String,
-  //updated_at: { type: Date, default: Date.now },
-});
-module.exports = mongoose.model('Todo', TodoSchema);

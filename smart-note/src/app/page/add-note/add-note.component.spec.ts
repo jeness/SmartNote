8449999786bb2f566f623c47b4660979a43ad5b0
Haoran Yu <yuhaoran9968@gmail.com>
2017-11-07 22:49:11 -0500

    Add sudo for mkdir in build.sh

--- smart-note/src/app/page/add-note/add-note.component.spec.ts
+++ smart-note/src/app/page/add-note/add-note.component.spec.ts
@@ -1,28 +1,28 @@
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-// import { FormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
-// import { AddNoteComponent } from './add-note.component';
+import { AddNoteComponent } from './add-note.component';
 
-// describe('AddNoteComponent', () => {
-//   let component: AddNoteComponent;
-//   let fixture: ComponentFixture<AddNoteComponent>;
+describe('AddNoteComponent', () => {
+  let component: AddNoteComponent;
+  let fixture: ComponentFixture<AddNoteComponent>;
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       imports: [FormsModule],
-//       declarations: [ AddNoteComponent ]
-//     })
-//     .compileComponents();
-//   }));
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [FormsModule],
+      declarations: [ AddNoteComponent ]
+    })
+    .compileComponents();
+  }));
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(AddNoteComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AddNoteComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//   it('should be created', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it('should be created', () => {
+    expect(component).toBeTruthy();
+  });
+});

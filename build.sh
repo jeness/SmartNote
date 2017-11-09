e55ec9d946ab9f8872271a069443c3900a675d61
CindyHXH <hxhcindy@gmail.com>
2017-11-09 00:29:18 -0500

    update travis

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 
 cd smart-note 
-npm install 
-npm install simplemde --save  
-npm install angular-highlightjs --save
-npm install -g mocha
-npm install chai
-npm install supertest --save-dev
+# npm install 
+# npm install simplemde --save  
+# npm install angular-highlightjs --save
+# npm install -g mocha
+# npm install chai
+# npm install supertest --save-dev
 
 mkdir -p /data/db
 chown -R `id -un` /data/db

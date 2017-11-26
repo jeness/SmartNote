983a56951e57f9e0c6f429c8b33d31f05589f151
CindyHXH <hxhcindy@gmail.com>
2017-11-26 18:26:28 -0500

    UPDATE MOCHA TEST

--- smart-note/test.js
+++ smart-note/test.js
@@ -35,7 +35,285 @@ describe('login', function() {
         setTimeout(done, 300);
     });   
 });
-
+describe('add note', function() {
+    it('should take less than 500ms', function(done){
+        this.timeout(500);
+        setTimeout(done, 300);
+    });
+    it('note cannot be empty',function(done){
+        request
+        .get('/api/user/1')
+        .expect(200)
+        .end(function(err, res){
+            if (err) return done(err);
+            var text = res.text;
+            var json = JSON.parse(text);
+            json.should.have.property('name');
+            json.should.have.property('password');
+            done();
+        });
+        this.timeout(500);
+        setTimeout(done, 300);
+    });
+    it('GET',function(done){
+        request
+        .get('/users')
+        .expect(200)
+        .end((err, res) => {
+        res.body.should.be.an.Array()
+        });
+        this.timeout(500);
+        setTimeout(done, 300);
+    });   
+});
+describe('add tag', function() {
+    it('should take less than 500ms', function(done){
+        this.timeout(500);
+        setTimeout(done, 300);
+    });
+    it('tag cannot be empty',function(done){
+        request
+        .get('/api/user/1')
+        .expect(200)
+        .end(function(err, res){
+            if (err) return done(err);
+            var text = res.text;
+            var json = JSON.parse(text);
+            json.should.have.property('name');
+            json.should.have.property('password');
+            done();
+        });
+        this.timeout(500);
+        setTimeout(done, 300);
+    });
+    it('GET',function(done){
+        request
+        .get('/users')
+        .expect(200)
+        .end((err, res) => {
+        res.body.should.be.an.Array()
+        });
+        this.timeout(500);
+        setTimeout(done, 300);
+    });   
+});
+describe('add Url note', function() {
+    it('should take less than 500ms', function(done){
+        this.timeout(500);
+        setTimeout(done, 300);
+    });
+    it('url note cannot be empty',function(done){
+        request
+        .get('/api/user/1')
+        .expect(200)
+        .end(function(err, res){
+            if (err) return done(err);
+            var text = res.text;
+            var json = JSON.parse(text);
+            json.should.have.property('name');
+            json.should.have.property('password');
+            done();
+        });
+        this.timeout(500);
+        setTimeout(done, 300);
+    });
+    it('GET',function(done){
+        request
+        .get('/users')
+        .expect(200)
+        .end((err, res) => {
+        res.body.should.be.an.Array()
+        });
+        this.timeout(500);
+        setTimeout(done, 300);
+    });   
+});
+describe('test search', function() {
+    it('should take less than 500ms', function(done){
+        this.timeout(500);
+        setTimeout(done, 300);
+    });
+    it('keyword cannot be empty',function(done){
+        request
+        .get('/api/user/1')
+        .expect(200)
+        .end(function(err, res){
+            if (err) return done(err);
+            var text = res.text;
+            var json = JSON.parse(text);
+            json.should.have.property('name');
+            json.should.have.property('password');
+            done();
+        });
+        this.timeout(500);
+        setTimeout(done, 300);
+    });
+    it('GET',function(done){
+        request
+        .get('/users')
+        .expect(200)
+        .end((err, res) => {
+        res.body.should.be.an.Array()
+        });
+        this.timeout(500);
+        setTimeout(done, 300);
+    });   
+});
+describe('check archived notes', function() {
+    it('should take less than 500ms', function(done){
+        this.timeout(500);
+        setTimeout(done, 300);
+    });
+    it('classification cannot be empty',function(done){
+        request
+        .get('/api/user/1')
+        .expect(200)
+        .end(function(err, res){
+            if (err) return done(err);
+            var text = res.text;
+            var json = JSON.parse(text);
+            json.should.have.property('name');
+            json.should.have.property('password');
+            done();
+        });
+        this.timeout(500);
+        setTimeout(done, 300);
+    });
+    it('GET',function(done){
+        request
+        .get('/users')
+        .expect(200)
+        .end((err, res) => {
+        res.body.should.be.an.Array()
+        });
+        this.timeout(500);
+        setTimeout(done, 300);
+    });   
+});
+describe('view note', function() {
+    it('should take less than 500ms', function(done){
+        this.timeout(500);
+        setTimeout(done, 300);
+    });
+    it('note id cannot be empty',function(done){
+        request
+        .get('/api/user/1')
+        .expect(200)
+        .end(function(err, res){
+            if (err) return done(err);
+            var text = res.text;
+            var json = JSON.parse(text);
+            json.should.have.property('name');
+            json.should.have.property('password');
+            done();
+        });
+        this.timeout(500);
+        setTimeout(done, 300);
+    });
+    it('GET',function(done){
+        request
+        .get('/users')
+        .expect(200)
+        .end((err, res) => {
+        res.body.should.be.an.Array()
+        });
+        this.timeout(500);
+        setTimeout(done, 300);
+    });   
+});
+describe('delete note', function() {
+    it('should take less than 500ms', function(done){
+        this.timeout(500);
+        setTimeout(done, 300);
+    });
+    it('note id cannot be empty',function(done){
+        request
+        .get('/api/user/1')
+        .expect(200)
+        .end(function(err, res){
+            if (err) return done(err);
+            var text = res.text;
+            var json = JSON.parse(text);
+            json.should.have.property('name');
+            json.should.have.property('password');
+            done();
+        });
+        this.timeout(500);
+        setTimeout(done, 300);
+    });
+    it('GET',function(done){
+        request
+        .get('/users')
+        .expect(200)
+        .end((err, res) => {
+        res.body.should.be.an.Array()
+        });
+        this.timeout(500);
+        setTimeout(done, 300);
+    });   
+});
+describe('modify note', function() {
+    it('should take less than 500ms', function(done){
+        this.timeout(500);
+        setTimeout(done, 300);
+    });
+    it('note id cannot be empty',function(done){
+        request
+        .get('/api/user/1')
+        .expect(200)
+        .end(function(err, res){
+            if (err) return done(err);
+            var text = res.text;
+            var json = JSON.parse(text);
+            json.should.have.property('name');
+            json.should.have.property('password');
+            done();
+        });
+        this.timeout(500);
+        setTimeout(done, 300);
+    });
+    it('GET',function(done){
+        request
+        .get('/users')
+        .expect(200)
+        .end((err, res) => {
+        res.body.should.be.an.Array()
+        });
+        this.timeout(500);
+        setTimeout(done, 300);
+    });   
+});
+describe('log out', function() {
+    it('should take less than 500ms', function(done){
+        this.timeout(500);
+        setTimeout(done, 300);
+    });
+    it('should be json',function(done){
+        request
+        .get('/api/user/1')
+        .expect(200)
+        .end(function(err, res){
+            if (err) return done(err);
+            var text = res.text;
+            var json = JSON.parse(text);
+            json.should.have.property('name');
+            json.should.have.property('password');
+            done();
+        });
+        this.timeout(500);
+        setTimeout(done, 300);
+    });
+    it('GET',function(done){
+        request
+        .get('/users')
+        .expect(200)
+        .end((err, res) => {
+        res.body.should.be.an.Array()
+        });
+        this.timeout(500);
+        setTimeout(done, 300);
+    });   
+});
 // describe('POST/tag', () => {
 //     it('should create a new tag', (done) => {
 //         var TestTag = new Tag({
